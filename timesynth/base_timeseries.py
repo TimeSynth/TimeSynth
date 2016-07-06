cb2731451717f3d89adf271de1f20a82a94e6d4d
1Reinier <reiniermaat@msn.com>
2016-07-06 12:17:14 -0400

    Added base implementations

---
+++ timesynth/base_timeseries.py
@@ -0,0 +1,11 @@
+class BaseTimeSeries:
+    
+    def __init__(self, signal, noise):
+        self.signal = signal
+        self.noise = noise
+        raise NotImplementedError
+    
+    def sample(n_samples):
+        self.samples = np.zeros(n_samples)
+        self.errors = np.zeros(n_samples)
+        raise NotImplementedError
