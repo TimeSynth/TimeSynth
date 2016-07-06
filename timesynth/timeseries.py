279dea7f4d20b7f4a65e698ef8528026c96283b7
1Reinier <reiniermaat@msn.com>
2016-07-06 14:51:03 -0400

    updates

--- timesynth/timeseries.py
+++ timesynth/timeseries.py
@@ -1,11 +1,22 @@
-class TimeSeries:
+class TimeSeriesGenerator:
     
-    def __init__(self, signal, noise):
-        self.signal = signal
-        self.noise = noise
-        raise NotImplementedError
+    def __init__(self, signal_generator, noise_generator):
+        self.signal_generator = signal_generator
+        self.noise_generator = noise_generator
     
-    def sample(n_samples):
-        self.samples = np.zeros(n_samples)
-        self.errors = np.zeros(n_samples)
-        raise NotImplementedError
+    def sample(self, n_samples, resolution):
+        samples = np.zeros(n_samples)  # Signal and errors combined
+        signals = np.zeros(n_samples)  # Signal samples
+        errors = np.zeros(n_samples)  # Handle errors seprately
+        
+        # Sample iteratively, while providing access to all previously sampled steps
+        for i in range(n_samples):
+            signals[i] = self.signal_generator.sample_next(samples[:i-1], errors[:i-1])
+            errors[i] = self.noise_generator.sample_next(samples[:i-1], errors[:i-1])
+            samples [i] = signals[i] + errors[i]
+            
+        return samples
+
+    def sample_range(self, start, stop, step):
+        num_samples = (stop - start) // step
+        return self.sample(num_samples, step)
