f11a0b71c255a0a0a7905ba93749bd18c29b1ed2
Abhishek Malali <abhishekmalali@gmail.com>
2016-07-06 12:14:26 -0700

    Completed sinusoidal curves

--- timesynth/signals/sinusoidal.py
+++ timesynth/signals/sinusoidal.py
@@ -1,30 +1,30 @@
 import numpy as np
 from base_signal import BaseSignal
 
-class Sinusoidal():
+class Sinusoidal(BaseSignal):
 
-    def __init__(self, amplitude=1.0, resolution=0.1):
+    def __init__(self, amplitude=1.0, resolution=0.1, ftype ="sin"):
+        if ftype not in ["sin", "cos", "tan"]:
+            raise ValueError, "ftype not correctly defined. Options are sin, cos and tan."
 
         self.amplitude = amplitude
         self.resolution = resolution
 
         self.curr_sample = -resolution
+        self.ftype_dict = {"sin": np.sin, "cos": np.cos, "tan": np.tan}
+        self.ftype = self.ftype_dict[ftype]
 
     def sample_next(self, samples, errors):
         self.curr_sample += self.resolution
-        return np.sin(self.curr_sample), self.curr_sample
+        return self.amplitude*self.ftype(self.curr_sample), self.curr_sample
 
 
     def sample(self, nsamples=100):
         timeVec = np.linspace(0, nsamples*self.resolution, num=nsamples)
-        signal = np.sin(timeVec)
+        signal = self.amplitude*self.ftype(timeVec)
 
         return signal, timeVec
 
-"""
-if __name__ == "__main__":
-    sig = Sinusoidal()
-    print sig.sample(nsamples=10)
-"""
+
 
 
