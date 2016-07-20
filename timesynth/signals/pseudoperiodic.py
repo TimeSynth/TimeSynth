113c76611a4a37dab004be6f9f90dabe13be71c9
1Reinier <reiniermaat@msn.com>
2016-07-20 15:37:22 -0400

    updates

--- timesynth/signals/pseudoperiodic.py
+++ timesynth/signals/pseudoperiodic.py
@@ -1,11 +1,12 @@
 import numpy as np
-from base_signal import BaseSignal
-import warnings
-warnings.filterwarnings("ignore")
+from .base_signal import BaseSignal
+
+__all__ = ['PseudoPeriodic']
+
 
 class PseudoPeriodic(BaseSignal):
 
-    def __init__(self, amplitude=1.0, frequency=100, ampSD = 0.1, freqSD = 0.4):
+    def __init__(self, amplitude=1.0, frequency=100, ampSD=0.1, freqSD=0.4):
         self.amplitude = amplitude
         self.frequency = frequency
         self.freqSD = freqSD
@@ -17,22 +18,21 @@ class PseudoPeriodic(BaseSignal):
         self.curr_sample += self.resolution
         freqVal = np.random.normal(loc=self.frequency, scale=self.freqSD, size=1)
         amplitudeVal = np.random.normal(loc=self.amplitude, scale=self.ampSD, size=1)
-        return float(amplitudeVal*np.sin(freqVal*self.curr_sample)), self.curr_sample
+        return float(amplitudeVal * np.sin(freqVal * self.curr_sample)), self.curr_sample
 
     def sample(self, nsamples=100):
 
         assert self.setFreqFlag == True, "Frequency not set for PseudoPeriodic object"
-        timeVec = np.linspace(0, nsamples*self.resolution, num=nsamples)
-        freqArr =  np.random.normal(loc=self.frequency, scale=self.freqSD, size=nsamples)
-        ampArr =  np.random.normal(loc=self.amplitude, scale=self.ampSD, size=nsamples)
+        timeVec = np.linspace(0, nsamples * self.resolution, num=nsamples)
+        freqArr = np.random.normal(loc=self.frequency, scale=self.freqSD, size=nsamples)
+        ampArr = np.random.normal(loc=self.amplitude, scale=self.ampSD, size=nsamples)
         signal = np.multiply(ampArr, np.sin(np.multiply(freqArr, timeVec)))
         return signal, timeVec
 
     def set_frequency(self, frequency):
-        self.resolution = 1./frequency
+        self.resolution = 1. / frequency
         self.curr_sample = -self.resolution
-        self.setFreqFlag = True 
-
+        self.setFreqFlag = True
 
 
 """
