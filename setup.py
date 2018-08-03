5232b629a9608bde58f2f8b3cb436816f677de30
Richard Pruss <boadie@gmail.com>
2018-08-03 12:08:07 +1000

    Fixed MackeyGlass signal to work with current jitcdde.
    Added a smoke test for MackeyGlass signal.
    Changed setup.py to use requirements.txt
    Added setup.cfg so that 'python setup.py test' works
    sympy was addded to requirements.txt as it is needed by jitcdde

--- setup.py
+++ setup.py
@@ -3,14 +3,23 @@ import logging
 from setuptools import setup
 from setuptools import find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+tests_requirements = [
+    'pytest'
+    ]
+
 setup(name='timesynth',
-      version='0.1',
+      version='0.2',
       description='Library for creating synthetic time series',
       url='https://github.com/TimeSynth/TimeSynth',
       author='Abhishek Malali, Reinier Maat, Pavlos Protopapas',
       author_email='anon@anon.com',
       license='MIT',
-      dependency_links=['git+https://github.com/neurophysik/jitcdde.git@697fb23#egg=jitcdde'],  # for jitcdde
       include_package_data=True,
       packages=find_packages(),
-      install_requires=['numpy', 'scipy', 'nose', 'jitcdde'])
+      install_requires=requirements,
+      tests_require=tests_requirements,
+      setup_requires=["pytest-runner"]
+      )
